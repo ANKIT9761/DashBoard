60b111c16df60b14e1c8820b6173df9ff8f40db9
Ankit Kanyal <ankitsinghkanyal43@gmail.com>
2023-02-01 17:54:58 +0530

    Completed the Simple Dashboard App.

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
+import "./SideBar.css";
+import Logo from "../../imgs/logo.png";
+import { UilSignOutAlt } from "@iconscout/react-unicons";
+import { SidebarData } from "../../Data/Data";
+import { UilBars } from "@iconscout/react-unicons";
+import { motion } from "framer-motion";
+
+const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
+
+  const [expanded, setExpaned] = useState(true);
+
+  const sidebarVariants = {
+    true: {
+      left: "0",
+    },
+    false: {
+      left: "-60%",
+    },
+  };
+  console.log(window.innerWidth);
+  return (
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
+    <>
+      <div
+        className="bars"
+        style={expanded ? { left: "60%" } : { left: "5%" }}
+        onClick={() => setExpaned(!expanded)}
+      >
+        <UilBars />
+      </div>
+      <motion.div
+        className="sidebar"
+        variants={sidebarVariants}
+        animate={window.innerWidth <= 768 ? `${expanded}` : ""}
+      >
+        {/* logo */}
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+          <span>
+            Sh<span>o</span>ps
+          </span>
+        </div>
+
+        <div className="menu">
+          {SidebarData.map((item, index) => {
+            return (
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+              <div
+                className={selected === index ? "menuItem active" : "menuItem"}
+                key={index}
+                onClick={() => setSelected(index)}
+              >
+                <item.icon />
+                <span>{item.heading}</span>
+              </div>
+            );
+          })}
+          {/* signoutIcon */}
+          <div className="menuItem">
+            <UilSignOutAlt />
+          </div>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Sidebar;
